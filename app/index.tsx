33b498b3f848be9ca80d8f41eae6f39e6a7f284a
wiliami <wiliaup97@gmail.com>
2025-01-10 17:23:03 -0400

    feat: new feature

--- app/index.tsx
+++ app/index.tsx
@@ -1,41 +1,10 @@
-import React, { useState } from 'react'
-import { View, Text, StyleSheet } from 'react-native'
-import { withGalio, GalioProvider, ThemeType } from 'galio-framework'
+import React from 'react'
+import { View, Text } from 'react-native'
 
-const customTheme = {
-  SIZES: { BASE: 18, },
-  // this will overwrite the Galio SIZES BASE value 16
-  COLORS: { PRIMARY: 'red', FACEBOOK: '#3B5998' } 
-  // this will overwrite the Galio COLORS PRIMARY color #B23AFC
-};
-
-interface IndexProps {
-  styles: {
-    container: {
-      flex: number,
-      backgroundColor: string
-    }
-  }  
-}
-
-
-function Index({ styles }: IndexProps) {
+export default function Index() {
     return ( 
-      <GalioProvider theme={customTheme}>
-        <View style={styles.container}>
+        <View>
           <Text>Olá, mundo</Text>
         </View>
-      </GalioProvider> 
     );
 }
-
-const createSyles = (theme: ThemeType)  => StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: theme.COLORS?.FACEBOOK || 'white'
-  }
-});
-
-// const styles = createSyles(customTheme) 
-
-export default withGalio(Index, createSyles)
